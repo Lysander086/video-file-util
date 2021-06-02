5da992b914e0af677fa15ff90ec9dadfc73a1283
Lysander <19286863+Lysander686@users.noreply.github.com>
2021-06-02 16:14:36 +0800

    no more occupied bug

--- java-impl/src/main/java/entry/BatchRenameFile.java
+++ java-impl/src/main/java/entry/BatchRenameFile.java
@@ -7,18 +7,19 @@ import java.io.File;
 
 /* "([|])" */
 public class BatchRenameFile {
-    //    public static final String containedKey = "( )"; // name to remove
-    public static final String[] containVal = new String[]{"[QQ微信352852792]"};
+
+    public static final String[] containValues = new String[]{" "};
+
     public static final boolean regexSupport = true;
     public static final String replacedVal = ""; // name to replace
-    public static String path = "C:\\Users\\rdc\\Desktop\\1fm\\java architecture course\\java架构"; // your video path here
+    public static String path = "C:\\Users\\inz\\Desktop\\1fm\\java architecture course\\bakRename - Copy"; // your video path here
     public static final boolean toGenerateFile = false; // whether create a text file in specified path
 
     public static void main(String[] args) {
         System.out.println("BatchRenameFile start");
         batchRename(path);
         System.out.println("BatchRenameFile end");
-        for (String containedKey : containVal) {
+        for (String containedKey : containValues) {
             if (toGenerateFile)
                 FileUtil.createFile(path + File.separator + "batch rename from " + containedKey + ".txt");
         }
@@ -30,27 +31,26 @@ public class BatchRenameFile {
         File[] files = file.listFiles();
 
         assert files != null;
-        String newName;
+
         File newNamedFile;
         for (File f : files) {
             if (f.isDirectory() && !f.getName().contains("git")) batchRename(f.getAbsolutePath());
-            String oldName = f.getName();
-            for (String containedVal : containVal) {
-                if (regexSupport || oldName.contains(containedVal)) {
-                    newName = oldName.replaceAll(containedVal, replacedVal);
-                    String fileNewName =
-                            f.getAbsolutePath().substring(0, f.getAbsolutePath().lastIndexOf(File.separator)) + File.separator + newName;
-                    newNamedFile = new File(fileNewName);
-                    if (f.renameTo(newNamedFile)) {
-                        if (regexSupport) if (!oldName.equals(newName))
-                            System.out.println("successfully get newNamedFile: " + fileNewName);
-                        else
-                            System.out.println("successfully get newNamedFile: " + fileNewName);
-                    } else {
-                        System.out.println(String.format("!!! Error: file %s may be in use, skipped renaming.", f.getName()));
-                    }
+
+            String newName = f.getName();
+            for (String containedVal : containValues) {
+                newName = newName.replaceAll(containedVal, replacedVal);
+            }
+            if (regexSupport || !newName.equals(f.getName())) {
+                String fileNewName =
+                        f.getAbsolutePath().substring(0, f.getAbsolutePath().lastIndexOf(File.separator)) + File.separator + newName;
+                newNamedFile = new File(fileNewName);
+                if (f.renameTo(newNamedFile)) {
+                    System.out.println("rename success: " + fileNewName);
+                } else {
+                    System.out.println(String.format("!!! Error: file %s may be in use, skipped renaming.", f.getName()));
                 }
             }
+
 
         }
     }
